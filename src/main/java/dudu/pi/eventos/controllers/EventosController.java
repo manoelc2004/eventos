ee6e05c3267c65d9d9f564c827ad1054cabbbdd9
Manoel Eduardo Câmara <eduardo.manoel@academico.ifrn.edu.br>
2022-10-27 16:38:22 -0300

    enviando dados do formulario para a aplicação

--- src/main/java/dudu/pi/eventos/controllers/EventosController.java
+++ src/main/java/dudu/pi/eventos/controllers/EventosController.java
@@ -2,6 +2,7 @@ package dudu.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class EventosController {
@@ -10,4 +11,9 @@ public class EventosController {
 	public String form() {
 	return "formevento";
 }
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento ) {
+		System.out.println(evento);
+		return "evento-adicionado";
+	}
 }
